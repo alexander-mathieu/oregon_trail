a1507b99f3dba9b2832a037334e5b58db89b5641
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-05-17 11:11:35 -0600

    Add caravan_names method to Node

--- lib/node.rb
+++ lib/node.rb
@@ -6,4 +6,29 @@ class Node
     @surname   = surname
     @next_node = nil
   end
+
+  def add_node(surname)
+    if @next_node.nil?
+      @next_node = Node.new(surname)
+    else
+      @next_node.add_node(surname)
+    end
+  end
+
+  def count(total)
+    if @next_node.nil?
+      return total
+    else
+      total += 1
+      @next_node.count(total)
+    end
+  end
+
+  def caravan_names(family_name)
+    if @next_node.nil?
+      "#{family_name}, followed by the #{@surname} family"
+    else
+      @next_node.caravan_names("#{family_name}, followed by the #{@surname} family")
+    end
+  end
 end

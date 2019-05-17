e149092618786d6c11d5aee99970bf8dc8fd0fc1
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-05-17 11:18:45 -0600

    Add specs for count and caravan_names instane methods

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -3,7 +3,7 @@ require './lib/node.rb'
 
 RSpec.describe Node do
   before :each do
-    @node = Node.new("Surname")
+    @node   = Node.new("Surname")
   end
 
   it "it should exist" do
@@ -16,5 +16,20 @@ RSpec.describe Node do
 
   it "it's next node is nil by default" do
     expect(@node.next_node).to eq(nil)
+  end
+
+  describe "instance methods" do
+    it "#count" do
+      @node_2 = @node.add_node("Surname 2")
+      @node_3 = @node.add_node("Surname 3")
+
+      expect(@node.count(1)).to eq(3)
+    end
+
+    it "#caravan_names" do
+      @node_2 = @node.add_node("Surname 2")
+
+      expect(@node.next_node.caravan_names("The #{@node.surname} family")).to eq("The Surname family, followed by the Surname 2 family")
+    end
   end
 end
